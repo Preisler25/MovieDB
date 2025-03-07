3bc77d90658155da5046c18d17073a9cd33d52b8
Preisler25 <preisler.andras@gmail.com>
2025-03-07 20:37:45 +0100

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Preisler András on 2025. 03. 07..
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieDBTests/MovieDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBTests.swift
+//  MovieDBTests
+//
+//  Created by Preisler András on 2025. 03. 07..
+//
+
+import Testing
+@testable import MovieDB
+
+struct MovieDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
