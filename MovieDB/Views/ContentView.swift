6b5c68f8561bb7940dd037d526ab575e3c89c1d3
Preisler25 <preisler.andras@gmail.com>
2025-03-10 14:25:52 +0100

    feat: Adding filtering buttons

--- MovieDB/Views/ContentView.swift
+++ MovieDB/Views/ContentView.swift
@@ -12,40 +12,61 @@ struct ContentView: View {
     @Environment(\.filmSettings) private var filmSettings
     var body: some View {
         VStack{
-            Text("MovieDB App")
+            Text("MovieDB App").font(.largeTitle)
             VStack{
-                Button(action: {
-                    changeType()
-                }){
-                    Text(filmSettings.type == FilmTypeEnum.movie ? "Movies" : "TV Shows")
-                }
-            }
-            ScrollViewReader { scrollView in
-                ScrollView(.horizontal){
-                    LazyHStack{
-                        if(filmSettings.page) > 1{
-                            Button(action: {
-                                changePage(cpt: .previous, scrollView: scrollView)
-                            }) {
-                                Label("Previous", systemImage: "arrow.left")
-                            }
-                        }
-                        ForEach(filmList.films, id: \.id) { film in
-                            FilmTileView(film: film).id(film.id)
+                Spacer()
+                VStack{
+                    HStack{
+                        Button(action: {
+                            changeType()
+                        }){
+                            Text(filmSettings.type == FilmTypeEnum.movie ? "Movies" : "TV Shows").font(.title2).padding(5).background().cornerRadius(6)
                         }
                         Button(action: {
-                            changePage(cpt: .next, scrollView: scrollView)
+                            changeType()
                         }){
-                            Label("Next", systemImage: "arrow.right")
+                            Text(filmSettings.type == FilmTypeEnum.movie ? "Movies" : "TV Shows").font(.title2).padding(5).background().cornerRadius(6)
                         }
-                    }.onAppear() {
-                        Task{
-                            await filmList.fetchFilms(filmSettings: filmSettings)
-                        }
-                    }.background(Color.gray)
+                        Toggle("Adult", isOn: Binding(
+                            get: { filmSettings.showAdult },
+                            set: {
+                                filmSettings.showAdult = $0
+                                Task {
+                                    await filmList.fetchFilms(filmSettings: filmSettings)
+                                }
+                            }
+                        )).frame(width: 100)
+                    }
                 }
-            }
-            
+                Spacer()
+                ScrollViewReader { scrollView in
+                    ScrollView(.horizontal){
+                        LazyHStack{
+                            if(filmSettings.page) > 1{
+                                Button(action: {
+                                    changePage(cpt: .previous, scrollView: scrollView)
+                                }) {
+                                    Label("Previous", systemImage: "arrow.left")
+                                }
+                            }
+                            ForEach(filmList.films, id: \.id) { film in
+                                FilmTileView(film: film).id(film.id)
+                            }
+                            Button(action: {
+                                changePage(cpt: .next, scrollView: scrollView)
+                            }){
+                                Label("Next", systemImage: "arrow.right")
+                            }
+                        }.onAppear() {
+                            Task{
+                                await filmList.fetchFilms(filmSettings: filmSettings)
+                            }
+                        }
+                    }
+                }.frame(height: 500)
+                Spacer()
+                Spacer()
+            }.background(Color.gray)
         }
     }
     
@@ -54,11 +75,6 @@ struct ContentView: View {
         case previous
     }
     
-    func changeType(){
-        filmSettings.type = filmSettings.type == FilmTypeEnum.movie ? FilmTypeEnum.tv : FilmTypeEnum.movie
-        
-        
-    }
     
     func changePage(cpt : changePageType, scrollView: ScrollViewProxy){
         switch cpt {
@@ -73,6 +89,22 @@ struct ContentView: View {
             withAnimation {
                 scrollView.scrollTo(filmList.films.first?.id, anchor: .leading)
             }
+        }
+    }
+    
+    func changeType(){
+        filmSettings.type = filmSettings.type == FilmTypeEnum.movie ? FilmTypeEnum.tv : FilmTypeEnum.movie
+        Task {
+            await filmList.fetchFilms(filmSettings: filmSettings)
+        }
+        
+    }
+    
+    
+    func changeIsAdult(){
+        filmSettings.showAdult.toggle()
+        Task {
+            await filmList.fetchFilms(filmSettings: filmSettings)
         }
     }
 }
